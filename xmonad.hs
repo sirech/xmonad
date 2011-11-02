8356a9bee828ee4a34a8d63513d49e3acf5c6215
Mario Fernandez <mario.fernandez@friendscout24.de>
2011-11-02 09:40:35 +0100

    Apps are shifted to specific workspaces

--- xmonad.hs
+++ xmonad.hs
@@ -18,50 +18,58 @@ myFocusedBorderColor = "#A0A0D0"
 
 -- WORKSPACES
 
-myWorkspaces = ["1:dev","2:web","3:mail","4:comm","5:ham","6:tmp","7:dvi","8","9","0","-","="]
+myWorkspaces = ["1:emacs","2:terminal","3:eclipse","4:eclipse-2","5:firefox","6:chrome","7:mail","8:pidgin","9:gitk","0","-","="]
 isFullscreen = (== "fullscreen")
 
--- Don't tile GNOME Do
-myManageHook :: [ManageHook]
-myManageHook = 
-    [ resource  =? "Do"   --> doIgnore ]
-    
-    
--- KEYBINDINGS    
+myManageHook :: ManageHook
+myManageHook = composeAll
+                 -- Don't tile GNOME Do
+               [ resource  =? "Do"   --> doIgnore
+               , className =? "Emacs" --> doShift "1:emacs"
+               , resource  =? "gnome-terminal" --> doShift "2:terminal"
+               , className =? "Firefox" --> doShift "5:firefox"
+               , className =? "Google-chrome" --> doShift "6:chrome"
+               , className =? "Thunderbird" --> doShift "7:mail"
+               , className =? "Pidgin" --> doShift "8:pidgin"
+               , className =? "Gitk" --> doShift "9:gitk"
+               ]
+
+
+-- KEYBINDINGS
 
 alt key = "M1-" ++ key
 win key = "M-" ++ key
 
-myKeys =                            
+myKeys =
   [ (win "<Space>", spawn "gnome-do")
   , (win "l", spawn "xflock4")
   , (win "`", sendMessage NextLayout)
     -- , ("M-S-`", setLayout $ XMonad.layoutHook baseConf)
-    
+
     -- use classic ALT+TAB
   , (alt "<Tab>", windows W.focusDown)
   , (alt "S-<Tab>", windows W.focusUp)
-                             
+
     -- moving workspaces
   , ("C-S-<Left>", prevWS)
   , ("C-S-<Right>", nextWS)
   , (win "S-<Left>", shiftToPrev)
   , (win "S-<Right>", shiftToNext)
-  ] 
+  ]
   ++
-  [ (alt k, windows (W.view space))    
+  [ (alt k, windows (W.view space))
   | space <- myWorkspaces, let k = [head space]]
 
 
 main :: IO ()
 main = xmonad $ baseConf
                            { terminal    = "gnome-terminal"
-                           , manageHook = manageHook baseConf <+> composeAll myManageHook
+                           , manageHook = manageHook baseConf <+> myManageHook
                            , modMask     = mod4Mask
                            , focusFollowsMouse = False
                            , workspaces = myWorkspaces
                            , borderWidth = myBorderWidth
                            , normalBorderColor = myNormalBorderColor
                            , focusedBorderColor = myFocusedBorderColor
-                           } 
+                           }
                            `additionalKeysP` myKeys
